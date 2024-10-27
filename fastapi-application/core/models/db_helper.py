a8cc8fd998764a371a45d3dee00e8ea33bdc7aee
Mikhail Artemov <babanlive@gmail.com>
2024-10-27 19:33:36 +0700

    changing app package name

--- fastapi-application/core/models/db_helper.py
+++ fastapi-application/core/models/db_helper.py
@@ -1,7 +1,8 @@
 from collections.abc import AsyncGenerator
 
-from core.config import settings
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+
+from core.config import settings
 
 
 class DatabaseHelper:
