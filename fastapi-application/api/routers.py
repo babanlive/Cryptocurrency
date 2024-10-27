f99c8d6c1278676fdd934111ee2b3518a7190b17
Mikhail Artemov <babanlive@gmail.com>
2024-10-27 20:02:25 +0700

    edited  prefix for router in routers.py

--- fastapi-application/api/routers.py
+++ fastapi-application/api/routers.py
@@ -1,12 +1,9 @@
-from core.config import settings
 from fastapi import APIRouter
 
 from .api_v1 import router as router_api_v1
 
 
-router = APIRouter(
-    prefix=settings.api.prefix,
-)
+router = APIRouter()
 router.include_router(
     router=router_api_v1,
 )
