99512319a6ff3ea8261e5af0d87169a8eb222239
Mikhail Artemov <babanlive@gmail.com>
2024-10-27 16:46:33 +0700

    added def add_price in crud/prices

--- fastapi-aplication/crud/prices.py
+++ fastapi-aplication/crud/prices.py
@@ -2,6 +2,7 @@ from collections.abc import Sequence
 from datetime import datetime
 
 from core.models import Price
+from core.schemas.prices import PriceCreate
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -22,3 +23,13 @@ async def get_latest_price(session: AsyncSession, ticker: str) -> Price | None:
     stmt = select(Price).where(Price.ticker == ticker).order_by(Price.timestamp.desc())
     result = await session.scalars(stmt)
     return result.first()
+
+
+async def add_price(session: AsyncSession, price_data: PriceCreate) -> None:
+    new_price = Price(
+        ticker=price_data.ticker,
+        price=price_data.price,
+        timestamp=price_data.timestamp,
+    )
+    session.add(new_price)
+    await session.commit()
