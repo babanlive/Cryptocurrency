697e1ec3a337974f16697bc4577ca829e5d92b82
Mikhail Artemov <babanlive@gmail.com>
2024-10-27 17:55:51 +0700

    added cheks ticket exists

--- fastapi-aplication/crud/prices.py
+++ fastapi-aplication/crud/prices.py
@@ -3,23 +3,27 @@ from datetime import datetime
 
 from core.models import Price
 from core.schemas.prices import PriceCreate
+from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_prices(session: AsyncSession, ticker: str) -> Sequence[Price]:
+    await ticker_exists(session, ticker)
     stmt = select(Price).where(Price.ticker == ticker)
     result = await session.scalars(stmt)
     return result.all()
 
 
 async def get_price_by_date(session: AsyncSession, ticker: str, date: datetime) -> Sequence[Price]:
+    await ticker_exists(session, ticker)
     stmt = select(Price).where(Price.ticker == ticker, Price.timestamp >= date)
     result = await session.scalars(stmt)
     return result.all()
 
 
 async def get_latest_price(session: AsyncSession, ticker: str) -> Price | None:
+    await ticker_exists(session, ticker)
     stmt = select(Price).where(Price.ticker == ticker).order_by(Price.timestamp.desc())
     result = await session.scalars(stmt)
     return result.first()
@@ -33,3 +37,10 @@ async def add_price(session: AsyncSession, price_data: PriceCreate) -> None:
     )
     session.add(new_price)
     await session.commit()
+
+
+async def ticker_exists(session: AsyncSession, ticker: str) -> None:
+    stmt = select(Price).where(Price.ticker == ticker).limit(1)
+    result = await session.scalar(stmt)
+    if result is None:
+        raise HTTPException(status_code=404, detail='Ticker not found')
