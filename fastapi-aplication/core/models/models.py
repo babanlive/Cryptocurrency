6b7dae1304483e6154e2e2ff8ec1cb16a1289ce3
Mikhail Artemov <babanlive@gmail.com>
2024-10-27 10:57:52 +0700

    create  models Price

---
+++ fastapi-aplication/core/models/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, Float, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class Price(Base):
+    __tablename__ = 'prices'
+
+    ticker: Mapped[str] = mapped_column(String, nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(DateTime, nullable=False)
